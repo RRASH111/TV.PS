d7068031d7a7e4af63b8b8f9c8cbee3e9da984f8
RRASH111 <ranishwaiki3@gmail.com>
2023-06-17 02:14:23 +0300

    add db, add prisma

--- tv.ps/app/auth/page.tsx
+++ tv.ps/app/auth/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import Input from "@/components/Input";
 
 
@@ -8,6 +8,13 @@ const Auth = () => {
   const [email,setEmail] = useState('');
   const [name,setName] = useState('');
   const [password,setPassword] = useState('');
+  
+  const [variant, setVariant] = useState('Login');
+
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) => currentVariant == 'login' ? 'register' : 'login')
+  }, [])
+
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
@@ -17,16 +24,18 @@ const Auth = () => {
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
             <h2 className="text-white text-4xl mb-8 font-semibold">
-              Sign in
+              {variant == 'login' ? 'Sing in' : 'Register'}
             </h2>
           
             <div className="flex flex-col gap-4" >
+              {variant == 'register' && (
               <Input
                 id="name"
                 onChange={(ev: any) => setName(ev.target.value)}
                 value={name}
                 lable="Username"
               />
+              )}
               <Input
                 id="email"
                 onChange={(ev: any) => setEmail(ev.target.value)}
@@ -43,12 +52,12 @@ const Auth = () => {
               />
             </div>
               <button className="bg-red-600 py-3 text-white rounded-md  w-full mt-10 hover:bg-red-900 transition-transform">
-                Login
+                {variant == 'login' ? 'login' : 'Sing up'}
               </button>
               <p className="text-neutral-500 mt-12">
-                First time Using TV.PS ?
-                <span className="text-white m1-1 hover:underline cursor-pointer">
-                   Create an account
+                {variant == 'login' ? 'First time Using TV.PS? ' : 'Already have an account? '}
+                <span onClick={toggleVariant} className="text-white m1-1 hover:underline cursor-pointer">
+                    {variant == 'login' ? 'Create an account' : 'login'}
                 </span>
               </p>
           </div>
