74591f9c448ed455c01257112f5dd963f5a543a1
RRASH111 <ranishwaiki3@gmail.com>
2023-06-17 00:11:58 +0300

    add auth hero.jpg

--- tv.ps/app/auth/page.tsx
+++ tv.ps/app/auth/page.tsx
@@ -17,8 +17,9 @@ const Auth = () => {
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
             <h2 className="text-white text-4xl mb-8 font-semibold">
-              Sing in
+              Sign in
             </h2>
+          
             <div className="flex flex-col gap-4" >
               <Input
                 id="name"
